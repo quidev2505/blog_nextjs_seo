4b67523b083369171e237579147fb2df07c4852d
--replace-all <quidev2505@gmail.com>
2024-08-28 11:12:31 +0700

    fix env

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const response = await fetch(`https://newsdata.io/api/1/latest?apikey=${process.env.API_KEYS}&country=us&category=sports,health,entertainment,technology`);
+  const response = await fetch(`https://newsdata.io/api/1/latest?apikey=${process.env.NEXT_PUBLIC_API_KEY}&country=us&category=sports,health,entertainment,technology`);
   const res = await response.json();
   const postEntries: MetadataRoute.Sitemap = res.results.map(({ article_id }: any) => ({
     url: `${process.env.NEXT_PUBLIC_BASE_URL}/blog/${article_id}`,
